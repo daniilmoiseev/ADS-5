8eefc308115bf17c58c1156e3848e07dfde53cf4
Daniil Moiseev <33635935+daniilmoiseev@users.noreply.github.com>
2021-04-28 23:26:37 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,12 +5,58 @@
 
 template<typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
+public:
+  TPQueue() :head(nullptr), tail(nullptr) {}
+  ~TPQueue() {
+    while (head)
+      pop();
+  }
+
+  void push(const T &item) {
+    T *t = new T;
+    t->ch = item.ch;
+    t->prior = item.prior;
+    t->next = nullptr;
+    if (tail && head) {
+      T *temp = head;
+      if (temp->prior < t->prior) {
+        t->next = temp;
+        head = t;
+      } else {
+        while (temp->next) {
+          if (temp->next->prior < t->prior) {
+            t->next = temp->next;
+            temp->next = t;
+            break;
+          } else {
+            temp = temp->next;
+          }
+        }
+      }
+      if (!temp->next) {
+        tail->next = t;
+        tail = tail->next;
+      }
+    } else {
+      head = t;
+      tail = head;
+    }
+  }
+  T pop() {
+    T *temp = head;
+    head = head->next;
+    return *temp;
+  }
+
+private:
+  T *head;
+  T *tail;
 };
 
 struct SYM {
   char ch;
-  int  prior;
+  int prior;
+  SYM *next;
 };
 
 #endif // INCLUDE_TPQUEUE_H_
